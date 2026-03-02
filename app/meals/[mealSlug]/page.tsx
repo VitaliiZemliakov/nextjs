b891ceb341720a726f01c8049715d915032f93ac
Vitalii Zemliakov <v.zemliakov@elogic.com.ua>
2026-03-02 17:32:51 +0200

    added a error to meals

--- app/meals/[mealSlug]/page.tsx
+++ app/meals/[mealSlug]/page.tsx
@@ -1,3 +1,45 @@
-export default function MealDetailsPage() {
-  return <h1>Meal Details</h1>;
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+// import { use } from "react";
+
+export default async function MealDetailsPage({
+    params,
+}: {
+    params: Promise<{ mealSlug: string }>;
+}) {
+    const mealSlug = (await params).mealSlug;
+    const meal = getMeal(mealSlug);
+
+    if (!meal) {
+        notFound()
+    }
+
+    const { instructions, title, creator_email, summary, creator } = meal;
+
+    return (
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal?.image} alt={title} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${creator_email}`}>{creator}</a>
+                    </p>
+                    <p className={classes.summary}>{summary}</p>
+                </div>
+            </header>
+            <main className={classes.main}>
+                <p
+                    className={classes.instructions}
+                    dangerouslySetInnerHTML={{
+                        __html: instructions.replace(/\n/g, "<br />"),
+                    }}
+                ></p>
+            </main>
+        </>
+    );
 }
--- lib/meals.tsx
+++ lib/meals.tsx
@@ -4,7 +4,12 @@ import sql from "better-sqlite3";
 const db = sql("meals.db");
 
 export async function getMeals(): Promise<Meal[]> {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 4000));
     // throw new Error('sorry, we have to stop you here')
-    return db.prepare('SELECT * FROM meals').all() as Meal[];
+    return db.prepare("SELECT * FROM meals").all() as Meal[];
+}
+
+export function getMeal(slug: string) {
+    //  await new Promise((resolve) => setTimeout(resolve, 4000));
+    return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug) as Meal;
 }
