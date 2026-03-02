7646aae5aef184a4d31d7af4decc237597495b84
Vitalii Zemliakov <v.zemliakov@elogic.com.ua>
2026-03-02 09:04:05 +0200

    Suspense with await

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -2,10 +2,15 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+import MealsLoading from "./loading";
+
+async function Meal() {
+    const meals = await getMeals();
+    return <MealsGrid meals={meals} />;
+}
 
 export default async function MealsPage() {
-   const meals = await getMeals();
-
     return (
         <>
             <header className={classes.header}>
@@ -22,7 +27,9 @@ export default async function MealsPage() {
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={meals} />
+                <Suspense fallback={<MealsLoading />}>
+                    <Meal />
+                </Suspense>
             </main>
         </>
     );
