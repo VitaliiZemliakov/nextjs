0eec5d7c6ce17072776d9ce8e89cf709caec0ad4
Vitalii Zemliakov <v.zemliakov@elogic.com.ua>
2026-03-05 19:04:27 +0200

    validations  fixed

--- lib/actions.tsx
+++ lib/actions.tsx
@@ -1,27 +1,28 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import {
-    extractMeal,
-    isInvalidText
-} from "./meal-utils";
+import { extractMeal, isInvalidText } from "./meal-utils";
 import { saveMeal } from "./meals";
-import { useActionState } from 'react'
 
-export async function shareMeal(formdata: FormData): Promise<void> {
+interface ActionState {
+    message: string | null;
+}
+
+export async function shareMeal(formdata: FormData): Promise<ActionState> {
     const meal = extractMeal(formdata);
-    useActionState()
 
-    for (const item in meal) {
-        if (item === "image") {
-            if ((meal[item] as File).size === 0) {
-                throw new Error(`${item} is required`);
-            }
-            continue;
-        }
-        if (isInvalidText(item)) {
-            throw new Error(`${item} is required`);
-        }
+    if (meal?.image.size === 0) {
+        return { message: "Invalid image" };
+    }
+
+    if (
+        isInvalidText(meal.title) ||
+        isInvalidText(meal.summary) ||
+        isInvalidText(meal.instructions) ||
+        isInvalidText(meal.creator) ||
+        isInvalidText(meal.creator_email)
+    ) {
+        return { message: "Invalid input" };
     }
 
     await saveMeal(meal);
