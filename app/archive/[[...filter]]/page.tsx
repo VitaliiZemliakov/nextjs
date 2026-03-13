0f54ac9e2d6bd919169b9e0f4a575a1f5f29ab12
Vitalii Zemliakov <vzemlyakov85@gmail.com>
2026-03-13 11:44:20 +0200

    implemented catch all dynamic segment

--- app/archive/[[...filter]]/page.tsx
+++ app/archive/[[...filter]]/page.tsx
@@ -1,27 +1,94 @@
-import { getAvailableNewsYears } from "@/api/news";
+import {
+    getAvailableNewsMonths,
+    getAvailableNewsYears,
+    getNewsForYear,
+    getNewsForYearAndMonth,
+} from "@/api/news";
 import Link from "next/link";
+import { MainHeaderSuspensed as NewsList } from "@/components/main-header";
 
-type Params = Promise<{ filter: [string] }>;
+type Params = Promise<{ filter: string[] }>;
 
-export default async function FilteredNewsPage(
-    { params }: { params: Params }
-) {
-    const filter = await params;
-    const links: number[] = getAvailableNewsYears();
+const monthsMap: Record<number, string> = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December",
+};
 
-    console.log(filter);
+export const noContent = <p>Sorry, no content found</p>;
 
-       return (
-        <header id="archive-header">
-            <nav>
-                <ul>
-                    {links.map((link) => (
-                        <li key={link}>
-                            <Link href={`/archive/${link}`}>{link}</Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
+export default async function FilteredNewsPage({ params }: { params: Params }) {
+    const { filter } = await params;
+    const linksYears: number[] = getAvailableNewsYears();
+
+    const [selectedYear, selectedMonth] = filter || [];
+
+    let news: ReturnType<typeof getNewsForYearAndMonth> = [];
+    let months: number[] = [];
+
+    if (selectedYear && !selectedMonth) {
+        news = getNewsForYear(parseInt(selectedYear));
+        months = getAvailableNewsMonths(parseInt(selectedYear));
+    }
+
+    if (selectedYear && selectedMonth) {
+        news = getNewsForYearAndMonth(
+            parseInt(selectedYear),
+            parseInt(selectedMonth),
+        );
+    }
+
+    let newsContent;
+
+    if (!selectedYear && !selectedMonth) {
+        newsContent = <p>Select a year to see news.</p>;
+    } else if (news && news.length > 0) {
+        newsContent = <NewsList data={news} classApplied="flex gap-4" />;
+    } else {
+        newsContent = <p>Sorry, no content found for the selected filter.</p>;
+    }
+
+    return (
+        <>
+            <header id="archive-header">
+                <nav>
+                    <ul>
+                        {linksYears.map((link) => {
+                            const href = `/archive/${link}`;
+
+                            return (
+                                <li key={link}>
+                                    <Link href={href}>{link}</Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                    {selectedYear && months.length > 0 && (
+                        <ul>
+                            {months?.map((month) => {
+                                const href = `/archive/${selectedYear}/${month}`;
+                                return (
+                                    <li key={month}>
+                                        <Link href={href}>
+                                            {monthsMap[month]}
+                                        </Link>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    )}
+                </nav>
+            </header>
+            {newsContent || noContent}
+        </>
     );
 }
