747cba211d0f8ea7032ecc98d70ecac06ef811d4
Vitalii Zemliakov <vzemlyakov85@gmail.com>
2026-03-17 11:51:44 +0200

    added intercepted route

---
+++ app/news/[newsSlug]/(.)image/page.tsx
@@ -0,0 +1,29 @@
+import { DUMMY_NEWS } from "@/api/dummy-news";
+import GoBackBtn from "@/components/go-back-btn";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+export default async function InterceptedImagePage({
+    params,
+}: {
+    params: Promise<{ newsSlug: string }>;
+}) {
+    const { newsSlug } = await params;
+
+    const newItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
+
+    if (!newItem) {
+        return notFound();
+    }
+
+    const imagePath = `/images/${newItem.category}/${newItem.image}`;
+    const imageAlt = newItem ? newItem.title : "News Image";
+
+    return (
+        <div className="fullscreen-image">
+            <h2>Intercepted</h2>
+            <Image src={imagePath} alt={imageAlt} width={800} height={600} />
+            <GoBackBtn />
+        </div>
+    );
+}
